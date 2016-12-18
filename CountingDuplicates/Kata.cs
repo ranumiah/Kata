88cae6af72c551e84b403f79d8f16eb0d17cfa0a
Ranu <ranu.miah@outlook.com>
2016-12-18 19:15:01 +0000

    Counting Duplicates - 01

--- CountingDuplicates/Kata.cs
+++ CountingDuplicates/Kata.cs
@@ -1,10 +1,58 @@
-﻿namespace CountingDuplicates
+﻿using System.Collections.Generic;
+
+namespace CountingDuplicates
 {
     public class Kata
     {
-        public static int DuplicateCount(string str)
+        public static int DuplicateCount(string word)
         {
-            return -1;
+            Dictionary<char, int> duplicateCounter = new Dictionary<char, int>();
+            int uniqueDuplicateLetters = 0;
+
+            if (string.IsNullOrEmpty(word))
+                return uniqueDuplicateLetters;
+            
+            foreach (char letter in GetLowerInvariantCharFor(word))
+            {
+                if (IsDuplicateLetter(duplicateCounter, letter))
+                {
+                    uniqueDuplicateLetters = UpdateDuplicateCounterForNewCharacter(duplicateCounter, letter, uniqueDuplicateLetters);
+                    IncrementDuplicateCount(duplicateCounter, letter);
+                }
+                else
+                {
+                    duplicateCounter.Add(letter, 1);
+                }
+            }
+            
+            return uniqueDuplicateLetters;
+        }
+
+        private static string GetLowerInvariantCharFor(string word)
+        {
+            return word.ToLowerInvariant();
+        }
+
+        private static bool IsDuplicateLetter(Dictionary<char, int> duplicateCounter, char letter)
+        {
+            return duplicateCounter.ContainsKey(letter);
+        }
+
+        private static int UpdateDuplicateCounterForNewCharacter(Dictionary<char, int> duplicateCounter, char letter, int duplicateCount)
+        {
+            if (IsItNewDuplicateLetter(duplicateCounter, letter))
+                duplicateCount++;
+            return duplicateCount;
+        }
+
+        private static bool IsItNewDuplicateLetter(Dictionary<char, int> duplicateCounter, char letter)
+        {
+            return duplicateCounter[letter] == 1;
+        }
+
+        private static void IncrementDuplicateCount(Dictionary<char, int> duplicateCounter, char letter)
+        {
+            duplicateCounter[letter]++;
         }
     }
 }
